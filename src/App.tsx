a303aa0d9ef4ac01ef758e0ab4f7e36ba301baa3
owen-stud123 <180330378+owen-stud123@users.noreply.github.com>
2026-09-11 21:39:05 +0200

    Render MyComponent in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import MyComponent from './MyComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MyComponent />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
