74d872664a215a4b451c0855da9d1e4c35cea0bd
owen-stud123 <180330378+owen-stud123@users.noreply.github.com>
2026-09-11 21:50:16 +0200

    Add styling to MyComponent

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,6 +1,8 @@
+import './MyComponent.css';
+
 function MyComponent() {
   return (
-    <div>
+    <div className="my-component">
       <h1>Hello from MyComponent!</h1>
       <p>This is a paragraph of text within my component.</p>
     </div>
