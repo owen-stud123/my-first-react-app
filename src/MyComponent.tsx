e08300eaaf3718e9407c99c8d428ad2f5a71f603
owen-stud123 <180330378+owen-stud123@users.noreply.github.com>
2026-09-11 21:27:48 +0200

    Create MyComponent

---
+++ src/MyComponent.tsx
@@ -0,0 +1,10 @@
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello from MyComponent!</h1>
+      <p>This is a paragraph of text within my component.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
